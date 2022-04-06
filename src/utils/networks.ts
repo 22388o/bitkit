79002ae89168ef9378d0b271eb51a3e72e667708
Corey Phillips <corey.lyle.phillips@gmail.com>
2022-04-06 19:34:00 -0400

    Updated networks in networks.ts.

--- src/utils/networks.ts
+++ src/utils/networks.ts
@@ -1,3 +1,5 @@
+import { networks as _networks } from 'bitcoinjs-lib';
+
 export type TAvailableNetworks = 'bitcoin' | 'bitcoinTestnet';
 
 export enum EAvailableNetworks {
@@ -26,28 +28,8 @@ Source: https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/src/networks.js
 List of address prefixes: https://en.bitcoin.it/wiki/List_of_address_prefixes
  */
 export const networks: INetworks = {
-	bitcoin: {
-		messagePrefix: '\x18Bitcoin Signed Message:\n',
-		bech32: 'bc',
-		bip32: {
-			public: 0x0488b21e,
-			private: 0x0488ade4,
-		},
-		pubKeyHash: 0x00,
-		scriptHash: 0x05,
-		wif: 0x80,
-	},
-	bitcoinTestnet: {
-		messagePrefix: '\x18Bitcoin Signed Message:\n',
-		bech32: 'tb',
-		bip32: {
-			public: 0x043587cf,
-			private: 0x04358394,
-		},
-		pubKeyHash: 0x6f,
-		scriptHash: 0xc4,
-		wif: 0xef,
-	},
+	bitcoin: _networks.bitcoin,
+	bitcoinTestnet: _networks.testnet,
 };
 
 //Returns an array of all available networks from the networks object.
