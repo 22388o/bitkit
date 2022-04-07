9f66c3fdde00bad08caa3c63f0fdb24243fba845
Corey Phillips <corey.lyle.phillips@gmail.com>
2022-04-07 15:49:37 -0400

    Updated DefaultNodeJsMethodsShape in nodejs-mobile/shapes.ts

--- src/utils/nodejs-mobile/shapes.ts
+++ src/utils/nodejs-mobile/shapes.ts
@@ -10,7 +10,81 @@ import {
 } from './types';
 import { v4 as uuidv4 } from 'uuid';
 
-export const DefaultNodeJsMethodsShape: {
+export const DefaultNodeJsMethodsShape = {
+	setup: (): INodeJsSetup => {
+		return {
+			id: uuidv4(),
+			method: ENodeJsMethods.setup,
+			data: {
+				mnemonic: '',
+				password: '',
+				selectedNetwork: undefined,
+			},
+		};
+	},
+	generateMnemonic: (): INodeJsGenerateMnemonic => {
+		return {
+			id: uuidv4(),
+			method: ENodeJsMethods.generateMnemonic,
+			data: {
+				strength: 256,
+			},
+		};
+	},
+	mnemonicToSeed: (): INodeJsMnemonicToSeed => {
+		return {
+			id: uuidv4(),
+			method: ENodeJsMethods.mnemonicToSeed,
+			data: {
+				mnemonic: '',
+				password: '',
+				selectedNetwork: undefined,
+			},
+		};
+	},
+	getScriptHash: (): INodeJsGetScriptHash => {
+		return {
+			id: uuidv4(),
+			method: ENodeJsMethods.getScriptHash,
+			data: {
+				address: '',
+				selectedNetwork: undefined,
+			},
+		};
+	},
+	getAddress: (): INodeJsGetAddress => {
+		return {
+			id: uuidv4(),
+			method: ENodeJsMethods.getAddress,
+			data: {
+				root: undefined,
+				path: '',
+				type: '',
+				selectedNetwork: undefined,
+			},
+		};
+	},
+	getMnemonicPhraseFromEntropy: (): INodeJsGetMnemonicPhraseFromEntropy => {
+		return {
+			id: uuidv4(),
+			method: ENodeJsMethods.getMnemonicPhraseFromEntropy,
+			data: {
+				entropy: undefined,
+			},
+		};
+	},
+	getSha256: (): INodeJsGetSha256 => {
+		return {
+			id: uuidv4(),
+			method: ENodeJsMethods.getSha256,
+			data: {
+				str: '',
+			},
+		};
+	},
+};
+
+/*export const DefaultNodeJsMethodsShape: {
 	setup: INodeJsSetup;
 	generateMnemonic: INodeJsGenerateMnemonic;
 	mnemonicToSeed: INodeJsMnemonicToSeed;
@@ -76,4 +150,4 @@ export const DefaultNodeJsMethodsShape: {
 			str: '',
 		},
 	},
-};
+};*/
